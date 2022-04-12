a6b5fd4ed89c5b954732002d4375dec81387525d
Patryk Szymik <patryk.szymik01@wp.pl>
2022-04-12 11:06:14 +0200

    Add abstract method and draw fractions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from abc import abstractclassmethod
+from abc import ABC, abstractmethod
 import pygame
 from pygame.locals import *
+import random
 import time
 
 WIDTH = 1000
@@ -10,61 +11,93 @@ FRACTION_LIST = ["Pirates", "Poland", "Sparrows",
                  "Avengers", "United", "Soccers", "Assassins", "Titans", "Students", "Idk"]
 
 
-class Ship:
-    def __init__(self, surface, name, color, fraction):
+class Ship(ABC):
+    def __init__(self, surface, name, color, fraction, speed):
         self.surface = surface
         self.name = name
         self.color = color
         self.fraction = fraction
+        self.speed = speed
+
+    @abstractmethod
+    def typeOfShip(self):
+        pass
 
     def sayHello(self):
         print(
-            f"Hi, my name is {self.name}, my color is {self.color}, my fraction is {self.fraction}, and I have speed equal to {self.speed}!")
+            f"Hi, my name is {self.name} and my class is {self.typeOfShip()}. My color is {self.color}, my fraction is {self.fraction}, and I have speed equal to {self.speed}!")
 
 
 class AircraftCarrier(Ship):  # Lotniskowiec
     def __init__(self, surface, name, fraction, speed=1, color="lightblue"):
         super().__init__(surface, name, color, fraction, speed)
 
+    def typeOfShip(self):
+        return "Lotniskowiec"
+
 
 class Battleship(Ship):  # Pancernik
     def __init__(self, surface, name, fraction, speed=2, color="brown"):
         super().__init__(surface, name, color, fraction, speed)
+
+    def typeOfShip(self):
+        return "Pancernik"
 
 
 class Cruiser(Ship):  # Krążownik
     def __init__(self, surface, name, fraction, speed=3, color="green"):
         super().__init__(surface, name, color, fraction, speed)
 
+    def typeOfShip(self):
+        return "Krążownik"
+
 
 class Destroyer(Ship):  # Niszczyciel
     def __init__(self, surface, name, fraction, speed=4, color="red"):
         super().__init__(surface, name, color, fraction, speed)
+
+    def typeOfShip(self):
+        return "Niszczyciel"
 
 
 class Submarine(Ship):  # Okręt podwodny
     def __init__(self, surface, name, fraction, speed=5, color="black"):
         super().__init__(surface, name, color, fraction, speed)
 
+    def typeOfShip(self):
+        return "Okręt podwodny"
+
 
 class Motorboat(Ship):  # Motorówka
     def __init__(self, surface, name, fraction, speed=6, color="white"):
         super().__init__(surface, name, color, fraction, speed)
+
+    def typeOfShip(self):
+        return "Motorówka"
 
 
 class Tanker(Ship):  # Tankowiec
     def __init__(self, surface, name, fraction, speed=1, color="grey"):
         super().__init__(surface, name, color, fraction, speed)
 
+    def typeOfShip(self):
+        return "Tankowiec"
+
 
 class PassengerFerry(Ship):  # Prom pasażerski
     def __init__(self, surface, name, fraction, speed=2, color="orange"):
         super().__init__(surface, name, color, fraction, speed)
 
+    def typeOfShip(self):
+        return "Prom pasażerski"
+
 
 class Hovercraft(Ship):  # Poduszkowiec
     def __init__(self, surface, name, fraction, speed=4, color="purple"):
         super().__init__(surface, name, color, fraction, speed)
+
+    def typeOfShip(self):
+        return "Poduszkowiec"
 
 
 class rockIsland:  # Wyspa skalna
@@ -85,7 +118,8 @@ class Game:
         self.drawGrid(15, 20)
         pygame.display.flip()
 
-        self.statek = Battleship(self.surface, "battleshipxd", "JackSparrows")
+        self.drawFraction()
+        self.statek = Motorboat(self.surface, "Pogromca", "Avengers")
         self.statek.sayHello()
 
     def drawGrid(self, rows, columns):
@@ -102,6 +136,19 @@ class Game:
             x += sizeBlockInX
             pygame.draw.line(self.surface, (255, 255, 255),
                              (x, 0), (x, HEIGHT))
+
+    def drawFraction(self):
+        self.fractions = []
+        firstFraction = FRACTION_LIST[random.randint(0, 9)]
+        self.fractions.append(firstFraction)
+        FRACTION_LIST.remove(firstFraction)
+        while len(self.fractions) < 3:
+            randomEnd = len(FRACTION_LIST) - 1
+            randomFraction = FRACTION_LIST[random.randint(0, randomEnd)]
+            self.fractions.append(randomFraction)
+            FRACTION_LIST.remove(randomFraction)
+
+        print(self.fractions)
 
     def run(self):
         running = True
